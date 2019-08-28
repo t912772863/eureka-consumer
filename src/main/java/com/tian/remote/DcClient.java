5f9adc73acac59471f40215068ce32cc382d9a28
tianxiong <tianxiong@youkeshu.com>
2019-08-28 17:08:52 +0800

    springboot 2.0版本.

--- src/main/java/com/tian/remote/DcClient.java
+++ src/main/java/com/tian/remote/DcClient.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * 由于引入了feign jar功能, 这里可以声明一个接口, 通过注解的方式, 与远程接口关联起来.
  * 这样本地在进行调用的时候可以类似于注入一个普通的service层
  * Created by Administrator on 2018/7/31 0031.
+ *
  */
 //@FeignClient("eureka-client")
 public interface DcClient {
